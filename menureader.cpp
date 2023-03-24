87d778350ca263f51d9590d9c74adbb02176b05f
Matteo Varalta <matteo.varalta@leonardo.com>
2023-03-24 16:55:28 +0100

    New format

--- menureader.cpp
+++ menureader.cpp
@@ -79,11 +79,11 @@ QMap<QDate, Menu> MenuReader::getWeekMenu(QDate day, int row, int col, int week)
 {
 	QMap<QDate, Menu> weekMenu;
 
-	weekMenu.insert(getDayMenu(firstMonday.addDays(0 + 7 * week), row, col + 1));
-	weekMenu.insert(getDayMenu(firstMonday.addDays(1 + 7 * week), row, col + 2));
-	weekMenu.insert(getDayMenu(firstMonday.addDays(2 + 7 * week), row, col + 3));
-	weekMenu.insert(getDayMenu(firstMonday.addDays(3 + 7 * week), row, col + 4));
-	weekMenu.insert(getDayMenu(firstMonday.addDays(4 + 7 * week), row, col + 5));
+	weekMenu.insert(getDayMenu(firstMonday.addDays(0 + 7 * week), row + 0, col));
+	weekMenu.insert(getDayMenu(firstMonday.addDays(1 + 7 * week), row + 7, col));
+	weekMenu.insert(getDayMenu(firstMonday.addDays(2 + 7 * week), row + 14, col));
+	weekMenu.insert(getDayMenu(firstMonday.addDays(3 + 7 * week), row + 21, col));
+	weekMenu.insert(getDayMenu(firstMonday.addDays(4 + 7 * week), row + 28, col));
 
 	return weekMenu;
 }
